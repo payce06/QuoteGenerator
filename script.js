ed8223b0bea19006a70ce670699da2059a501d01
payce06 <paycegrossman@gmail.com>
2025-04-30 21:08:52 -0400

    script-js

---
+++ script.js
@@ -0,0 +1,8 @@
+const quotes = [
+    { quote: "The best way to get started is to quit talking and begin doing.", author: "Walt Disney" },
+    { quote: "Don't watch the clock; do what it does. Keep going.", author: "Sam Levenson" },
+    { quote: "Success is not in what you have, but who you are.", author: "Bo Bennett" },
+    { quote: "Opportunities don't happen. You create them.", author: "Chris Grosser" },
+    { quote: "Everything you can imagine is real.", author: "Pablo Picasso" },
+    { quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" }
+];
